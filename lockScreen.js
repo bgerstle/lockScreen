c78d47b2bd0d799b82eb0605d6c592100659423a
Brian Gerstle <brian.gerstle@gmail.com>
2013-09-22 21:59:43 -0400

    initial commit w/ hello world working
    Change-Id: I25030c5292dc5fb6abc009d4965b23db0d4ab669

---
+++ lockScreen.js
@@ -0,0 +1,27 @@
+var animationProgress = 0.0;
+var animationInc = 1.0/60.0;
+
+function animateSVGGradient (el) {
+    console.log(el);
+    animationProgress += animationInc;
+    if (animationInc > 0) {
+        animationProgress = Math.min(0.99, animationProgress);
+    } else {
+        animationProgress = Math.max(0.01, animationProgress);
+    }
+    el.setAttribute('x2', animationProgress.toString());
+    if (animationProgress == 0.99 || animationProgress == 0.01) {
+        animationInc *= -1.0;
+    }
+    console.log('animating gradient ' + animationProgress.toString());
+
+    // TODO: bind
+    requestAnimationFrame(function () { 
+        animateSVGGradient(el);
+    });
+}
+
+window.onload = function () {
+    // var gradient = document.getElementById('g');
+    // animateSVGGradient(gradient);
+};
